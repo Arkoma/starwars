a4b144b273c38e969779c26a49a78a9ecf76bfe3
Arkoma <aaron@adburk.net>
2018-02-15 15:48:34 -0600

    changes to index.js to use react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Link, Route, NavLink } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route path="/" component={App} exact/>
+        </div>
+    </Router>
+    , document.getElementById('root'));
